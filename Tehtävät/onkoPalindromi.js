46d476be43d888a6cf6046204dedf6464e7f42e1
JoniLaakkonen <89970474+JoniLaakkonen@users.noreply.github.com>
2022-12-11 17:28:19 +0200

    Delete Tehtävät directory

--- Tehtävät/onkoPalindromi.js
+++
@@ -1,9 +0,0 @@
-function onkoPalindromi(str){
-    let str = prompt("Kirjoita jokin merkkijono ja tarkistan onko se palindromi: ")
-    let teststr = str.split('').reverse().join('');
-    if(str == teststr){
-        return true;
-    }
-    return false;
-}
-onkoPalindromi();
